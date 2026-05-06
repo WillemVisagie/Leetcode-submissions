6144a8c6229af546bb60da90e890648ddd7b4969
Willem Visagie <66644812+WillemVisagie@users.noreply.github.com>
2026-05-06 15:36:25 +0200

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int[] s1Count = new int[26];
+        int[] s2Count = new int[26];
+        if (s1.length() > s2.length()) return false;
+        for (int i = 0; i < s1.length(); i++) {
+            s1Count[s1.charAt(i) - 'a'] ++;
+            s2Count[s2.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < s2.length() - s1.length(); i++) {
+            if (Arrays.equals(s1Count, s2Count)) return true;
+            s2Count[s2.charAt(i + s1.length()) - 'a']++;
+            s2Count[s2.charAt(i) - 'a']--;
+        }
+
+        return Arrays.equals(s1Count, s2Count);
+    }
+}
